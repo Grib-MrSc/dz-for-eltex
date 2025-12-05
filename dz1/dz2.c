a6a3d5703e6bfe44e327c5a2456838eb4b9ebb48
Grib-MrSc <ilya-romanchenko@mail.ru>
2025-12-05 22:25:13 +0700

    Add files via upload
    второе задание

---
+++ dz1/dz2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int number,pro;
+    printf("input positive integer number");
+    printf("\n");
+    pro=scanf("%d",&number);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    if (pro!=1){
+        printf("inputed char,not  a nubmer, please input a number");
+        printf("\nprint Enter for exit");
+        getchar();
+        return 1;
+    }
+    if (number<0) {
+    printf("incorrect input, number must be positive!");
+    printf("\nprint Enter for exit");
+    getchar();
+    return 1;
+    }
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (number >> i) & 1);
+    }
+    printf("\nprint Enter for exit");
+    getchar();
+    return 0;
+}
