09511ce0bf52458d90cf30f993a9c7a5c39e947a
Grib-MrSc <ilya-romanchenko@mail.ru>
2025-12-10 13:54:04 +0700

    Update dz2.c

--- dz1/dz2.c
+++ dz1/dz2.c
@@ -6,20 +6,22 @@ int main()
     printf("\n");
     pro=scanf("%d",&number);
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    while ((c = getchar()) != '\n' && c != EOF); //очистка буфера
     if (pro!=1){
         printf("inputed char,not  a nubmer, please input a number");
         printf("\nprint Enter for exit");
         getchar();
         return 1;
     }
-    if (number<0) {
+    if (number<0) //проверка на ввода положительного числа
+    {
     printf("incorrect input, number must be positive!");
     printf("\nprint Enter for exit");
     getchar();
     return 1;
     }
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 31; i >= 0; i--) 
+    {
         printf("%d", (number >> i) & 1);
     }
     printf("\nprint Enter for exit");
