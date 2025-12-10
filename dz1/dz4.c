28e47fe9da47d11b41e293dc2d7ff7cbda6c5311
Grib-MrSc <ilya-romanchenko@mail.ru>
2025-12-10 13:55:11 +0700

    Update dz4.c

--- dz1/dz4.c
+++ dz1/dz4.c
@@ -8,12 +8,12 @@ int main() {
     printf("input number: ");
     scanf("%u", &num);
     
-    while ((c = getchar()) != '\n' && c != EOF);
+    while ((c = getchar()) != '\n' && c != EOF);//очистка буфера для удержания командной строки(отдельное окно)
     
     printf("input new value of 3 byte: ");
     scanf("%hhu", &new_value);
     
-    num = (num & 0xFF00FFFF) | (new_value << 16);
+    num = (num & 0xFF00FFFF) | (new_value << 16);//очищаем 2 байта(с 16 по 31 бит числа num)
     printf("result: %u\n", num);
     
     printf("Press Enter to exit...");
