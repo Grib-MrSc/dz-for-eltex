0cc95dd284692b92334dba8aa36ccc1836b6c4b5
Grib-MrSc <ilya-romanchenko@mail.ru>
2025-12-10 13:54:49 +0700

    Update dz3.c

--- dz1/dz3.c
+++ dz1/dz3.c
@@ -6,14 +6,16 @@ int main()
     printf("\n");
     pro=scanf("%d",&number);
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-    if (pro!=1){
+    while ((c = getchar()) != '\n' && c != EOF);//очистка буфера 
+    if (pro!=1) //проверка ввода числа, а не символа
+    {
         printf("inputed char,not  a nubmer, please input a number");
         printf("\nprint Enter for exit");
         getchar();
         return 1;
     }
-    if (number<0) {
+    if (number<0) //проверка положительности числа
+    {
     printf("incorrect input, number must be positive!");
     printf("\nprint Enter for exit");
     getchar();
@@ -22,10 +24,11 @@ int main()
     int val = number;
     while (val > 0) 
     {
-        if (val & 1) { 
-            count++;
+        if (val & 1) //если побитовом представлении встречается 1(логическое &1) 
+        { 
+            count++; //- записываем в счетчик +1
         }
-        val >>= 1; 
+        val >>= 1; // сдвигаемся на бит дальше
     }
     printf("number of '1'  bits :%d", count);
     printf("\nprint Enter for exit");
